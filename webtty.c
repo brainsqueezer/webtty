303ce76188eacb41b7faba9b460bdc8b00a36580
Ramon Antonio Parada <ramon@bigpress.net>
2011-11-24 11:44:28 +0100

    update for current gcc

--- webtty.c
+++ webtty.c
@@ -27,6 +27,8 @@ Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include <sys/types.h>
 #include <sys/time.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <pty.h>
 #include <unistd.h>
 #include <fcntl.h>
